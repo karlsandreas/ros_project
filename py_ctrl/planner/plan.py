460d04a1c349eb07ec00922c5fe0722966658902
Andreas Karlsson <andreask413@gmail.com>
2021-11-10 16:34:22 +0100

    Prestarting works as expected

--- py_ctrl/planner/plan.py
+++ py_ctrl/planner/plan.py
@@ -7,10 +7,7 @@ from model.operation import Operation, Transition
 from predicates.state import State
 from predicates.guards import Guard
 from predicates.actions import Action
-""" 
 
-def plan(state: State, goal: Guard, model: Model, max_depth: int = 20) -> Optional[List[str]]:
-"""
 """
     Find a sequence of operations to reach the goal from the given state or
     return None if you can not find a plan. Use max_depth to stop searching when you have more than
@@ -23,40 +20,14 @@ def plan(state: State, goal: Guard, model: Model, max_depth: int = 20) -> Option
 
     In the runner, there is a mode to pre-start operations, but that should not be considered while planning
 """
-"""
-    order=[]
-    goal
 
-    while not goal.eval(state):
-        stack=[state]
-        controled_stack=[state]
-        while stack and len(stack)<max_depth:
-            for i in stack:
-                
-                stack.pop(0)
-                
-                for op in model.operations:
-                    if (model.operations[op].eval(i) and model.operations[op].next_planning(i) not in controled_stack):
-                        
-                        if goal.eval(model.operations[op].next_planning(i)):
-                            order.insert(0,model.operations[op].name)
-                            goal=model.operations[op].precondition.guard
-                            break
-                        stack.append(model.operations[op].next_planning(i))
-                        controled_stack.append(model.operations[op].next_planning(i))
-        if not order:
-            return None
-
-    return order
-
-"""
 
 def plan(state: State, goal: Guard, model: Model, max_depth: int = 20) -> Optional[List[str]]:
     stack = []
     state_op_weight = [state,[],0]
     stack.append(state_op_weight) #stack is a nested list with state and operations to that state
 
-    visited = []
+    visited = set()
     while stack:
         current_state = stack.pop(0) 
         if len(current_state[1])>max_depth:
@@ -79,14 +50,13 @@ def plan(state: State, goal: Guard, model: Model, max_depth: int = 20) -> Option
                         print("visited states, ", len(visited))
                         return ops_copy  #return list of operations
 
-                    elif next_state not in stack:
+                    elif state_copy[0] not in stack:
 
                         state_ops_weights = [next_state,ops_copy,weight]
                         stack.append(state_ops_weights)
                     
                 state_copy = current_state.copy()
-            visited.append(current_state[0])
-        stack = sorted(stack, key=lambda x:x[2])
+            visited.add(current_state[0])
     print("Number of visited ", len(visited))
     print("Stack empty")
     return None
